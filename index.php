f6e2cb042b1f26c08d4903bf1e0fa3e8c5a619a5
Henry <143923152+fgccomtw@users.noreply.github.com>
2023-10-11 21:52:04 +0800

    Update index.php
    EMAIL

--- index.php
+++ index.php
@@ -1,7 +1,18 @@
-<?php     
-$to_email ='henry.fgc@gmail.com';
-$subject = 'Testing PHP Mail';
-$message = 'This mail is sent using the PHP mail function';
-$headers = 'From: service@fgc.com.tw';
-mail($to_email,$subject,$message,$headers);
+<?php
+// 判斷是否需要發送郵件
+if(isset($_REQUEST['email']) && strlen($_REQUEST['email']) > 5) 
+{
+    // 取得資訊
+    $admin_email = "aaronlife@gmail.com";
+    $header = "From:".$_REQUEST['email']."\r\n";
+    $name = $_REQUEST['name'];
+    $message = $_REQUEST['message'];
+
+    // 發送郵件
+    if(FALSE == mail($admin_email, "由[".$name."]傳來的訊息", $message, $header))
+    echo "<h2 class=\"align-center\">發送失敗！</h2>";
+    else
+    echo "<h2 class=\"align-center\">謝謝您的聯絡！</h2>";
+    echo "<br/>";
+}
 ?>
